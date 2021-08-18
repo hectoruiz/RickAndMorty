77e80609f4c046b76aec665d9c471aa967ce5cad
hectoruiz <hectoruiz.89@gmail.com>
2021-08-18 14:12:44 +0200

    Add retrofit Create api client and api service Minor fixes

--- datasource/src/main/java/hector/ruiz/datasource/api/ApiClient.kt
+++ datasource/src/main/java/hector/ruiz/datasource/api/ApiClient.kt
@@ -1,3 +1,16 @@
 package hector.ruiz.datasource.api
 
-class ApiClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class ApiClient {
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    companion object {
+        const val BASE_URL = "https://rickandmortyapi.com/api/"
+    }
+}
